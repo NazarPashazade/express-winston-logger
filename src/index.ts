0f50e3a1d59e134ca93831fef23d43cb3afdf58f
Nazar Pashazade <pashazade.nazar@gmail.com>
2025-09-05 18:47:16 +0300

    adjusting files

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,7 @@
-import { format, createLogger as winstonCreateLogger } from "winston";
-import { createHttpLogger } from "./http-logger";
 import { LogLevel } from "./types/log-level";
-import { LoggerOptions } from "./types/logger-options";
-import {
-  createConsoleTransport,
-  createElasticsearchTransport,
-} from "./transports";
 
-export function createLogger({
-  serviceName,
-  elasticsearchUrl,
-  level = LogLevel.INFO,
-  enableConsole = true,
-  enableElasticsearch = true,
-}: LoggerOptions) {
-  const transportsList: any[] = [];
+export type { LoggerOptions } from "./types/logger-options";
 
-  if (enableConsole) {
-    transportsList.push(createConsoleTransport(level));
-  }
+export { createLogger } from "./logger";
 
-  if (enableElasticsearch) {
-    transportsList.push(
-      createElasticsearchTransport({
-        level,
-        url: elasticsearchUrl,
-        serviceName,
-      })
-    );
-  }
-
-  const logger = winstonCreateLogger({
-    level,
-    defaultMeta: { service: serviceName },
-    transports: transportsList,
-    format: format.combine(
-      format.timestamp({ format: () => new Date().toISOString() }),
-      format.errors({ stack: true }),
-      format.splat(),
-      format.json()
-    ),
-  });
-
-  const httpLogger = createHttpLogger(logger);
-
-  return { logger, httpLogger };
-}
+export { LogLevel };
